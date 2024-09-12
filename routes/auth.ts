e3fe727e0bd572b4665f649ede9b47ea422ba177
Dorra <dorra@Islams-iMac-Pro.local>
2024-09-12 21:58:13 +0100

    finished login and signup routes

--- routes/auth.ts
+++ routes/auth.ts
@@ -2,12 +2,23 @@ import { Router } from 'express';
 import { login, loginWithPhoneOtp, logout, signup } from '../controllers/auth';
 import { validateDataWithZod } from '../middlewares/validateData';
 import { userLoginSchema, userRegistrationSchema } from '../schemas/userSchema';
+import { rateLimit } from 'express-rate-limit';
+
+const limiter = rateLimit({
+  windowMs: 5 * 60 * 1000,
+  limit: 2,
+});
 
 const router = Router();
 
-router.post('/login', validateDataWithZod(userLoginSchema), login);
+router.post('/login', limiter, validateDataWithZod(userLoginSchema), login);
 
-router.post('/signup', validateDataWithZod(userRegistrationSchema), signup);
+router.post(
+  '/signup',
+  limiter,
+  validateDataWithZod(userRegistrationSchema),
+  signup
+);
 
 router.post('/verifyOtp', loginWithPhoneOtp);
 
